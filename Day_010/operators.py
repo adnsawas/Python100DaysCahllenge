4ffc927c53ea9a85e5e19aad437af96f6d2e36e1
adnsawas <adnsawas@gmail.com>
2019-08-29 16:52:03 +0300

    Completed exercise of day 10

---
+++ Day_010/operators.py
@@ -0,0 +1,30 @@
+# Arithmetic operations
+print(3 + 5)
+print(3 - 5)
+print(3 * 5)
+print(6 / 3)
+print(5 % 3)
+print(8 ** 2)
+print(7 // 3)
+print(3 + 5)
+
+# Assignment Operations
+x = 1
+x += 3
+print(x)
+
+# Bitwise Operators
+print(0 & 1) # AND
+print(0 | 1) # OR
+print(0 ^ 1) # XOR
+print(0 >> 1) # RIGHT SHIFT
+print(0 << 1) # LEFT SHIFT
+print(~ 1) # INVERSE
+
+# Logical Operators
+print(1 > 0)
+print(1 < 0)
+print(1 >= 0)
+print(1 <= 0)
+print(1 == 0)
+print(1 != 0)
