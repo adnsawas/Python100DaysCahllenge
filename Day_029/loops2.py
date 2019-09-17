b190e94a727f6e2b1a52083c6aee6345bfd31dad
adnsawas <adnsawas@gmail.com>
2019-09-17 22:03:57 +0300

    Completed exercise of day 29

---
+++ Day_029/loops2.py
@@ -0,0 +1,12 @@
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(fruit)
+    if fruit == "banana":
+        break
+
+for c in "CHARACHTERS":
+    if c == "C":
+        continue
+    else:
+        print(c)
+
